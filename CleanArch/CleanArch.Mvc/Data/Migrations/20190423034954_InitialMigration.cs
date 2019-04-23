b7ff293afa073581c06ff313ac0caba2d5f318b8
innovativeinstitution <innovativeinstitution@gmail.com>
2019-04-22 21:05:59 -0700

    Initial Commit for CleanArch.Mvc Project (Presentation)

---
+++ CleanArch/CleanArch.Mvc/Data/Migrations/20190423034954_InitialMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArch.Mvc.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
