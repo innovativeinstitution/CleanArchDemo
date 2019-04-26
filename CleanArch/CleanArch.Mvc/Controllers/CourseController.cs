553f1ab665b01639ea033cc8fec5bdf96dc7945b
innovativeinstitution <innovativeinstitution@gmail.com>
2019-04-25 23:27:39 -0700

    Commit changes for Presentation Course Controller, Views, Stying, Authentication

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
